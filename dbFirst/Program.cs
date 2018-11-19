a1a0c2ea6bfbe9abe18a7ef1b050af6fb8b72d50
B.Nasrdinov <donnaragorn@gmail.com>
2018-11-19 15:11:54 +0500

    Add project files.

---
+++ dbFirst/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dbFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (TESTEntities db = new TESTEntities())
+            {
+                var users = db.Users;
+                foreach (User u in users)
+                    Console.WriteLine("{0}.{1} - {2}", u.ID, u.Name, u.Age);
+            }
+        }
+    }
+}
